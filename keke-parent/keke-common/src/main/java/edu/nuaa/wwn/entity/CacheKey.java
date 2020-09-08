4c9b2a9543e3bd6ed6d6893961abe72ea3f021e9
wuweinan <573877858@qq.com>
2020-09-08 16:47:05 +0800

    feat: 一个商城网站，完成框架创建

---
+++ keke-parent/keke-common/src/main/java/edu/nuaa/wwn/entity/CacheKey.java
@@ -0,0 +1,26 @@
+package edu.nuaa.wwn.entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: WeinanWu
+ * Date: 2020-09-08
+ * Time: 16:21
+ */
+public class CacheKey {
+
+    /**
+     * 商品分类
+     */
+    public static String CATEGORY = "CATEGORY";
+
+    /**
+     * 品牌缓存
+     */
+    public static String BRAND = "BRAND";
+
+    /**
+     * 规格
+     */
+    public static String SPEC = "SPEC";
+}
---
+++ keke-parent/keke-eureka/src/main/java/edu/nuaa/wwn/EurekaApplication.java
@@ -0,0 +1,21 @@
+package edu.nuaa.wwn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: WeinanWu
+ * Date: 2020-09-08
+ * Time: 16:10
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
